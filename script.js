34363b8bea1d2e3fe4df2bbc409912e536f2889b
Julien <dev.julien.f@gmail.com>
2019-09-29 22:29:21 +0200

    creation variables

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const buttonPrevious = document.querySelector(".btn1");
+const buttonNext = document.querySelector(".btn2");
+const li = document.querySelectorAll("li");
+let position = 0;
+
+function nextImage() {
+    position++; 
+    if (position === li.length) {
+        position = 0;
+    }      
+    for (let i = 0; i < li.length; i++) {
+        li[i].classList.remove("active");
+    }   
+    li[position].classList.add("active");
+}
+
+function previousImage() {
+    position--; 
+    if (position === li.length) {
+        position = 0;
+    }      
+    for (let i = 0; i < li.length; i++) {
+        li[i].classList.remove("active");
+    } 
+    li[position].classList.add("active");    
+}
+
+buttonNext.addEventListener("click", nextImage);
+buttonPrevious.addEventListener("click", previousImage);
+// li.addEventListener("click", () => alert("ok"));
+// elem.addEventListener("click", () => alert ("ok"));
